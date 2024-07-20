96da1fe62c00f504d4e797cde0e27391871e834e
ChrissyNambz <christine.nambatya@gmail.com>
2024-07-20 22:32:33 +0100

    Added Search API

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 function refreshWeather(response) {
-  console.log(response.data.current.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+  let climateSearch = document.querySelector("#climate-city");
+  climateSearch.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = math.round(temperature);
 }
 
 function recieveCity(cityValue) {
   let apiKey = "t6468120b34dd22a5f93b7f863a2oc0a";
   let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${cityValue} &key= ${apiKey}&units=metric";
+    'https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}&units=metric';
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let citySearch = document.querySelector("#enter-city");
-  let climateSearch = document.querySelector("#climate-city");
-  climateSearch.innerHTML = citySearch.value;
+  // let climateSearch = document.querySelector("#climate-city");
+  // climateSearch.innerHTML = citySearch.value;
   recieveCity(citySearch.value);
 }
 
 let searchFormElement = document.querySelector("#search-city");
-
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+Searc
